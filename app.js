dbbfc315188cefff28bc47dec234da23c5bf246d
fmfahath <fmfahath.dev@gmail.com>
2024-06-10 23:40:37 +0530

    public path added

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const exhbs = require('express-handlebars')
 const loginPage = require('./controllers/userController')
+const path = require('path');
 
 //handlebars engine
 app.engine('hbs', exhbs.engine({ extname: 'hbs', defaultLayout: false, }))
 app.set('view engine', 'hbs')
+
+//setting public folder path
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', loginPage)
 
